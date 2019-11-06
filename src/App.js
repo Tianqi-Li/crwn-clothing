0414d880b9c85bde3de1ab6201454f099839ddbe
Tianqi Li <tianqili@Tianqis-MacBook-Pro.local>
2019-11-05 20:33:32 -0800

    added sign in and sign up components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/shop/ShopPage';
 import Header from './components/header/Header';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/SignInAndSignUpPage';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
