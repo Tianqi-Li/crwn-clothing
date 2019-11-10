0716ef0bbfa9e0276a80ee0017b3a9b440ecb3f2
Tianqi Li <tianqili@Tianqis-MacBook-Pro.local>
2019-11-10 09:51:57 -0800

    implemented cartItem reducer action and components

--- src/components/cart-dropdown/CartDropdown.js
+++ src/components/cart-dropdown/CartDropdown.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import CustomButton from '../custom-button/CustomButton';
+import CartItem from '../cart-item/CartItem';
 
 import './CartDropdown.scss';
 
 class CartDropdown extends React.Component {
   render() {
+    const {cartItems} = this.props;
     return (
       <div className="cart-dropdown">
-        <div className="cart-items" />
+        <div className="cart-items">
+          {
+            cartItems.map(item => <CartItem key={item.id} item={item} />)
+          }
+        </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
       </div>
     );
   }
 }
 
-export default CartDropdown;
+const mapStateToProps = ({cart: {cartItems}}) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
