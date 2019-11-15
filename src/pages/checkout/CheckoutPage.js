721affd2aa6ec9968413dd7cbc7af34776bc1e9a
Tianqi Li <tianqili@Tianqis-MacBook-Pro.local>
2019-11-14 16:46:42 -0800

    stripe integration and updated favicon and title

--- src/pages/checkout/CheckoutPage.js
+++ src/pages/checkout/CheckoutPage.js
@@ -5,6 +5,7 @@ import {createStructuredSelector} from 'reselect';
 import {selectCartItems, selectCartTotal} from '../../redux/cart/CartSelectors';
 
 import CheckoutItem from '../../components/checkout-item/CheckoutItem';
+import StripeCheckoutButton from '../../components/stripe-button/StripeCheckoutButton';
 
 import './CheckoutPage.scss';
 
@@ -33,6 +34,12 @@ const CheckoutPage = ({cartItems, total}) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br/ >
+      4242 4242 4242 4242 - Exp: 01/20 CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
