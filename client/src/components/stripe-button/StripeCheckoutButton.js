81e969716e02087214d61256316851b8ee05372f
Tianqi Li <tianqili@Tianqis-MacBook-Pro.local>
2019-11-19 13:51:43 -0800

    build express server to handle payment with Stripe

---
+++ client/src/components/stripe-button/StripeCheckoutButton.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({price}) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_HhC4QhW1uGymWbxhj6QxejFv009ZTOE7Pj';
+
+  const onToken = token => {
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    }).then(response => {
+      alert('Payment successfull');
+    }).catch(error => {
+      console.log('Payment error: ', error);
+      alert('There is an issue with the payment');
+    });
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+}
+
+export default StripeCheckoutButton;
